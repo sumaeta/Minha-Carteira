a960bef264fc346d3ee4e0b37f7de2b6410663cb
sumaeta <brenosumaeta33@gmail.com>
2021-06-17 20:36:34 -0300

    Configurando os estilos globais

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Dashboard from './pages/Dashboard';
+import App from './app';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Dashboard />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
