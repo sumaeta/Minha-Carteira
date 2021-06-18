9521e1c6937230b2ed7bdf02c0e7af775671d357
sumaeta <brenosumaeta33@gmail.com>
2021-06-18 17:41:16 -0300

    Definindo espaços com o CSS grid

---
+++ src/components/Content/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from './styles';
+
+const Component: React.FC = () => {
+    return (
+        <Container>
+             <h1>Component</h1>
+        </Container>
+       
+    )
+}
+
+export default Component;
